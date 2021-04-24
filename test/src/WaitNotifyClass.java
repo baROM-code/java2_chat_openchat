637b257778f2f29a06b81b5d30cc8f19cc24030a
Roman Bar <barom64@mail.ru>
2021-04-24 10:53:13 +0400

    1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.

---
+++ test/src/WaitNotifyClass.java
@@ -0,0 +1,73 @@
+public class WaitNotifyClass {
+
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        WaitNotifyClass waitNotifyObj = new WaitNotifyClass();
+        Thread thread1 = new Thread(() -> {
+            waitNotifyObj.printA();
+        });
+        Thread thread2 = new Thread(() -> {
+            waitNotifyObj.printB();
+        });
+        Thread thread3 = new Thread(() -> {
+            waitNotifyObj.printC();
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
